c3d1996aabd4db704710041506bbcc66a6013751
maddy3019 <madhura2205@yahoo.com>
2017-04-04 11:15:31 -0500

    StringToInteger second commit

--- StringToInteger.java
+++ StringToInteger.java
@@ -5,12 +5,13 @@ public class StringToInteger {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter String:");
-		String str = sc.nextLine();
+		String str = sc.nextLine();// input string
 		int answer = 0, factor = 1;
-	    for (int i = str.length()-1; i >= 0; i--) {
-	        answer += (str.charAt(i) - '0') * factor;
-	        factor *= 10;
-	    }
+		for (int i = str.length() - 1; i >= 0; i--) {
+			// convert each character by ASCII value to integer
+			answer += (str.charAt(i) - '0') * factor;
+			factor *= 10;
+		}
 		System.out.println(answer);
 	}
 
